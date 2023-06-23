ea87c765e7c220c1c9afa0687790b8f59c31e10b
Peter Munro <peter@petermunro.org>
2023-06-23 18:06:25 +0100

    Corrected GenericsDemo

--- exercises/collections/src/main/java/GenericsDemo.java
+++ exercises/collections/src/main/java/GenericsDemo.java
@@ -4,10 +4,9 @@ public class GenericsDemo {
     public static void main(String[] args) {
         Person p = new Person("Ben", 27, 14);
         doSomething(p);
-
     }
 
-    static public <T extends Comparator<Person>> void doSomething(T thing) {
+    static public <T extends Comparable<Person>> void doSomething(T thing) {
         System.out.println(thing);
     }
 
